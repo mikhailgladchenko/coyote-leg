5fe1bb1766b5d7883b9d044c8b0e092c80f41252
Mikhail_Gladchenko <mikhail_gladchenko@epam.com>
2017-12-18 16:15:39 -0800

    PEP8 inspected code

--- week1/matrix_done.py
+++ week1/matrix_done.py
@@ -2,7 +2,6 @@ class Matrix:
     def __init__(self, matrix_list):
         self.rows = matrix_list
 
-
     def __str__(self):
         return '\n'.join(' '.join(map(str, sl)) for sl in self.rows)
 
