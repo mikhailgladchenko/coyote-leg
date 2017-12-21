8f3713531df63a5074cb6ee6fc3b2bef8190b2c6
mikhailgladchenko <34400497+mikhailgladchenko@users.noreply.github.com>
2017-12-21 11:46:14 -0800

    adding solutions for string1,list1,wordcount and matrix (#2)
    * adding solutions for string1,list1,wordcount and matrix
    
    * some changes to get_dictionary function and changes with more meaninful varibles names.
    
    * removing unnessary file
    
    * PEP8 inspected code
    
    * implemented duplicate functions not using lambdas,PEP8 inspected code
    
    * implemented _str__ ,PEP8 inspected code
    
    * PEP8 inspected code
    
    * implementtion with Counter class,PEP8 inspected code
    
    * quicksort implementation,PEP8 inspected code
    
    * quicksort implementation,PEP8 inspected code
    
    * with changes in print_top function
    
    * with changes to remove print function
    
    * PEP8 inspected code
    
    * PEP8 inspected code
    
    * implemented match_ends_without_lambda function with list comprehension,PEP8 expected code
    
    * implemented isvalid method to check the validity of matrices for multiplication,PEP8 expected code
    
    * solution for task1 from week2
    
    * solution for task2 from week2
    
    * solution for task4 from week2

---
+++ week2/ex1_done.py
@@ -0,0 +1,31 @@
+# 1.	Write a function that will parse a url query string like "page=2&foo=bar&x=y&utm_source=ss&utm_content=ccc" and return a query string where all the params are sorted by their names like this "foo=bar&page=2&x=y" and all the "utm_" parameters are removed.
+
+
+def parse_url(url):
+    string_splitted = url.split("&")
+    params_dict = {}
+    for entry in string_splitted:
+        key = entry.split("=")[0]
+        value = entry.split("=")[1]
+        if "utm_" not in key:
+            params_dict[key] = value
+    keys_sorted = sorted(params_dict.keys())
+    new_url = ""
+
+    for key in keys_sorted:
+
+        new_url += "{}={}{}".format(key, params_dict[key], "&")
+
+    return new_url[0:-1]
+
+
+def main():
+    url = "page=2&foo=bar&x=y&utm_source=ss&utm_content=ccc"
+    print(url)
+
+    url_new = parse_url(url)
+    print(url_new)
+
+
+if __name__ == '__main__':
+    main()
