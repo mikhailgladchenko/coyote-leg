d6d8537504df8aeb47ff5f389c942ade2e0c2603
Mikhail_Gladchenko <mikhail_gladchenko@epam.com>
2018-01-09 15:25:33 -0800

    added new unit test case

--- week2/ex1_done.py
+++ week2/ex1_done.py
@@ -9,8 +9,10 @@ def parse_url(url):
     else:
         string_splitted = url.split("&")
         filtered = [x for x in string_splitted if not x.startswith("utm_")]
-        sorted_alpha = sorted(filtered, key=lambda k: k[0])
-        url_new = "&".join(sorted_alpha)
+        splitted = [x.split("=") for x in filtered]
+        sorted_alpha = sorted(splitted, key=lambda k: k[0])
+
+        url_new = "&".join("{}={}".format(k, v) for k, v in sorted_alpha)
         return url_new
 
 
@@ -18,8 +20,11 @@ class TestUrlMethods(unittest.TestCase):
     def test_url_empty(self):
         self.assertEqual(parse_url(""), None)
 
-    def test_url_not_empty(self):
+    def test_url_not_empty1(self):
         self.assertEqual(parse_url("page=2&foo=bar&x=y&utm_source=ss&utm_content=ccc"), "foo=bar&page=2&x=y")
+
+    def test_url_not_empty2(self):
+        self.assertEqual(parse_url("apple=2&axile=bar&abbey=y&utm_source=ss&utm_content=ccc"), "abbey=y&apple=2&axile=bar")
 
 
 if __name__ == '__main__':
