5fad1cee25e7a73bf33c3e5ee928b7ba4c4ee5c6
Mikhail_Gladchenko <mikhail_gladchenko@epam.com>
2018-01-09 15:26:10 -0800

    added new unit test case

--- week2/ex2_done.py
+++ week2/ex2_done.py
@@ -18,8 +18,11 @@ class TestUrlMethods(unittest.TestCase):
     def test_url_empty(self):
         self.assertEqual(parse_url(""), None)
 
-    def test_url_not_empty(self):
+    def test_url_not_empty1(self):
         self.assertEqual(parse_url("page=2&foo=bar&x=y&utm_source=ss&utm_content=ccc"), "page=2&foo=bar&x=y")
+
+    def test_url_not_empty2(self):
+        self.assertEqual(parse_url("apple=2&axile=bar&abbey=y&utm_source=ss&utm_content=ccc"), "apple=2&axile=bar&abbey=y")
 
 
 if __name__ == '__main__':
