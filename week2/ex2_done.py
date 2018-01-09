2d7c3c10b8f957fce04b2fe4edaa2ab6e3131c39
Mikhail_Gladchenko <mikhail_gladchenko@epam.com>
2018-01-09 09:44:54 -0800

    changes with removed dictionary and unit tests added

--- week2/ex2_done.py
+++ week2/ex2_done.py
@@ -1,36 +1,26 @@
 # 2.	Write a function that will parse a url query string like "page=2&foo=bar&x=y&utm_source=ss&utm_content=ccc" and return a query string where all the params retain their original order "page=2&foo=bar&x=y" and all the "utm_" parameters are removed.
 
+import unittest
+
 
 def parse_url(url):
-
-    string_splitted = url.split("&")
-    params_dict = {}
-    filtered = [x for x in string_splitted if not x.startswith("utm_")]
-    for entry in filtered:
-        key = value = ""
-        try:
-            key = entry.split("=")[0]
-            value = entry.split("=")[1]
-        except IndexError:
-            pass
-
-        params_dict[key] = value
-
-    url_new = "&".join(["%s=%s" % (k, v) for k, v in params_dict.items()])
-
-    if url_new == str("="):
+    if url == "":
         return None
     else:
+        string_splitted = url.split("&")
+        filtered = [x for x in string_splitted if not x.startswith("utm_")]
+
+        url_new = "&".join(filtered)
         return url_new
 
 
-def main():
-    url = "page=2&foo=bar&x=y&utm_source=ss&utm_content=ccc"
-    print(url)
-    url_new = parse_url(url)
-    print(url_new)
-    print(parse_url(""))
+class TestUrlMethods(unittest.TestCase):
+    def test_url_empty(self):
+        self.assertEqual(parse_url(""), None)
+
+    def test_url_not_empty(self):
+        self.assertEqual(parse_url("page=2&foo=bar&x=y&utm_source=ss&utm_content=ccc"), "page=2&foo=bar&x=y")
 
 
 if __name__ == '__main__':
-    main()
+    unittest.main()
