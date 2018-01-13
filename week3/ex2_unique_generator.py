8feb45247915f5097ad3fb1225074093fca9ebdb
Mikhail_Gladchenko <mikhail_gladchenko@epam.com>
2018-01-13 08:30:53 -0800

    ex2 of week 3

--- week3/ex2_unique_generator.py
+++ week3/ex2_unique_generator.py
@@ -1,20 +1,19 @@
 
-def unique(mylist=[]):
-    set = {}
-    unique_values=[]
+def unique(mylist):
+    dict_unique = {}
+    unique_values = []
     if mylist is not []:
-        unique_values = [set.setdefault(e, e) for e in mylist if e not in set]
-    i=0
-    while i <len(unique_values):
-        yield unique_values[i]
-        i+=1
+        unique_values = [dict_unique.setdefault(e, e) for e in mylist if e not in dict_unique]
+
+    for v in unique_values:
+        yield v
 
 
 if __name__ == '__main__':
     lst = [1, 2, 0, 1, 4, 1, 0, 5, 2]
-    l=list(unique(lst))
-    print(l==[1,2,0,4,5])
-    lst1=[]
-    l1=list(unique([]))
+    ret = list(unique(lst))
+    print(ret == [1, 2, 0, 4, 5])
+    lst1 = []
+    l1 = list(unique([]))
     print(l1)
 
