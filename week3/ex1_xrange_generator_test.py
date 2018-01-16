b3a1b30a0941f1c381ff14093617837e46e64018
Mikhail_Gladchenko <mikhail_gladchenko@epam.com>
2018-01-16 14:57:17 -0800

    added a new test case from Yan's comments

--- week3/ex1_xrange_generator_test.py
+++ week3/ex1_xrange_generator_test.py
@@ -16,7 +16,7 @@ class TestxrangeIterator(unittest.TestCase):
     def test_xrange_join(self):
         self.assertEqual(",".join(str(i) for i in xrange(0,10)),"0,1,2,3,4,5,6,7,8,9")
     def test_xrange_list4(self):
-        self.assertEqual(list(xrange(10,0)),[10, 9, 8, 7, 6, 5, 4, 3, 2, 1])
+        self.assertEqual(list(xrange(10,0,-1)),[10, 9, 8, 7, 6, 5, 4, 3, 2, 1])
     def test_exception1(self):
         with self.assertRaises(TypeError):
             ret = list(xrange(0, 1, 0.1))
@@ -24,8 +24,10 @@ class TestxrangeIterator(unittest.TestCase):
         with self.assertRaises(TypeError):
             ret = list(xrange(0, 10,2, 2 ))
     def test_xrange_list5(self):
-        self.assertEqual(list(xrange(10,0,-1)),[10, 9, 8, 7, 6, 5, 4, 3, 2, 1])
+        self.assertEqual(list(xrange(10,0,-1)),[10,9, 8, 7, 6, 5, 4, 3, 2, 1])
     def test_xrange_list6(self):
         self.assertEqual(list(xrange(10)),[0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
+    def test_xrange_yan1(self):
+        self.assertEqual(list(xrange(10,2)),list(range(10,2)))
 if __name__ == '__main__':
     unittest.main()
