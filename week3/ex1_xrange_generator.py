08df6426e83549f285c3739219d7e4a30e51c6b7
Mikhail_Gladchenko <mikhail_gladchenko@epam.com>
2018-01-16 14:56:04 -0800

    code change to satisfy suggested test case

--- week3/ex1_xrange_generator.py
+++ week3/ex1_xrange_generator.py
@@ -1,6 +1,6 @@
 def xrange(*args):
-    for i in list(args):
-        if not (isinstance(i,int)):
+    for arg  in args:
+        if not (isinstance(arg,int)):
             raise TypeError("object cannot be interpreted as an integer")
     current = 0
     high = 0
@@ -9,25 +9,23 @@ def xrange(*args):
 
         current = 0
         high = args[0]
-        step = 1
+        #step = 1
     elif len(args) == 2:
+
         current = args[0]
-        high = args[1]
-        step = 1 if current < high else -1
+        high= args[1]
+        #step = 1 if current < high else -1
     elif len(args) == 3:
         current = args[0]
         high = args[1]
-        step = args[2] if current < high else -1
+        #step = args[2] if current < high else -1
+        step = args[2]
     else:
         raise TypeError("xrange expected at most 3 arguments, got %s" % len(args))
-    if current < high:
-        while current < high:
-            yield current
-            current += step
-    else:
-        while high < current:
-            yield current
-            current += step
+
+    for i in range(current, high,step):
+        yield i
+
 
 
 if __name__ == '__main__':
@@ -35,5 +33,13 @@ if __name__ == '__main__':
     print(list(xrange(2, 20)))
     print(list(xrange(0, 20, 2)))
     print(list(xrange(-10, 0, 1)))
-    print(list(xrange(10, 0, -1)))
-    print(list(xrange(10, 0)))
+    print(list(xrange(10,2)))
+    print(list(xrange(0, 10,2)))
+    #print(list(xrange(10, 0, -1)))
+    #print(list(xrange(0, 10, 2)))
+
+
+
+
+    #print(list(xrange(10, 0, -1)))
+
