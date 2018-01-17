10b5d7a1eeab07669f3bd695c1b20a73afd8d44e
Mikhail_Gladchenko <mikhail_gladchenko@epam.com>
2018-01-17 13:20:02 -0800

    PEP8 compliant

--- week3/ex1_xrange_generator.py
+++ week3/ex1_xrange_generator.py
@@ -1,6 +1,6 @@
 def xrange(*args):
-    for arg  in args:
-        if not (isinstance(arg,int)):
+    for arg in args:
+        if not (isinstance(arg, int)):
             raise TypeError("object cannot be interpreted as an integer")
     current = 0
     high = 0
@@ -9,23 +9,22 @@ def xrange(*args):
 
         current = 0
         high = args[0]
-        #step = 1
+
     elif len(args) == 2:
 
         current = args[0]
-        high= args[1]
-        #step = 1 if current < high else -1
+        high = args[1]
+
     elif len(args) == 3:
         current = args[0]
         high = args[1]
-        #step = args[2] if current < high else -1
+
         step = args[2]
     else:
         raise TypeError("xrange expected at most 3 arguments, got %s" % len(args))
 
-    for i in range(current, high,step):
+    for i in range(current, high, step):
         yield i
-
 
 
 if __name__ == '__main__':
@@ -33,8 +32,8 @@ if __name__ == '__main__':
     print(list(xrange(2, 20)))
     print(list(xrange(0, 20, 2)))
     print(list(xrange(-10, 0, 1)))
-    print(list(xrange(10,2)))
-    print(list(xrange(0, 10,2)))
+    print(list(xrange(10 , 2)))
+    print(list(xrange(0, 10, 2)))
     #print(list(xrange(10, 0, -1)))
     #print(list(xrange(0, 10, 2)))
 
