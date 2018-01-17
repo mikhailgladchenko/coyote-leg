7449f5411567a1b39e9467806fbe374349209aa6
Mikhail_Gladchenko <mikhail_gladchenko@epam.com>
2018-01-17 07:36:32 -0800

    unit tests for permutation generator

--- week3/ex4_permutations_test.py
+++ week3/ex4_permutations_test.py
@@ -6,23 +6,23 @@ import itertools
 class TestPermutations(unittest.TestCase):
     def test_perms1(self):
         data = [1, 2, 3]
-        self.assertEqual(permutations(data), list(itertools.permutations(data)))
+        self.assertEqual(list(permutations(data)), list(itertools.permutations(data)))
 
     def test_perms2(self):
         data = "123"
-        self.assertEqual(permutations(data), list(itertools.permutations(data)))
+        self.assertEqual(list(permutations(data)), list(itertools.permutations(data)))
 
     def test_perms3(self):
         data = ["a", "b", "c"]
-        self.assertEqual(permutations(data), list(itertools.permutations(data)))
+        self.assertEqual(list(permutations(data)), list(itertools.permutations(data)))
 
     def test_perms4(self):
         data = []
-        self.assertEqual(permutations(data), list(itertools.permutations(data)))
+        self.assertEqual(list(permutations(data)), list(itertools.permutations(data)))
 
     def test_perms5(self):
         data = [1]
-        self.assertEqual(permutations(data), list(itertools.permutations(data)))
+        self.assertEqual(list(permutations(data)), list(itertools.permutations(data)))
 
 
 if __name__ == '__main__':
