925f832cb37ace55e62535718d9b219412fd3646
Mikhail_Gladchenko <mikhail_gladchenko@epam.com>
2018-01-22 12:28:56 -0800

    PEP8 compliant code

--- week3/ex2_unique_generator.py
+++ week3/ex2_unique_generator.py
@@ -9,7 +9,7 @@ if __name__ == '__main__':
     lst = [1, 2, 0, 1, 4, 1, 0, 5, 2]
     ret = list(unique(lst))
     print(ret == [1, 2, 0, 4, 5])
-    lst1 = []
+    lst_empty = []
     l1 = list(unique([]))
-    print(l1)
+    print( lst_empty)
 
